5c2fd2b077b66598c15ceb703d8c80a666f59629
GieMik <giemik@gmail.com>
2017-11-26 19:00:18 +0200

    Setup style and fonts
    - SCSS setup
    - Fonts init

--- api/routes/beer_routes.js
+++ api/routes/beer_routes.js
@@ -6,40 +6,7 @@ module.exports = function(app) {
 			return;
 		}
     
-    console.log('getting beers')
-		const response = {  
-      "items":[  
-        {  
-          "id":"1",
-          "name":"Šviesusis",
-          "bitterness":3,
-          "color":"light",
-          "alc":6
-        },
-        {  
-          "id":"2",
-          "name":"Tamsusis",
-          "bitterness":5,
-          "color":"dark",
-          "alc":8
-        },
-        {  
-          "id":"3",
-          "name":"Karamelinis",
-          "bitterness":4,
-          "color":"dark",
-          "alc":7
-        },
-        {  
-          "id":"4",
-          "name":"Kvietinis",
-          "bitterness":3,
-          "color":"light",
-          "alc":4
-        }
-      ],
-      "itemCount":4
-		}
+		const response = require('../../models/beers')
 
 		res.send(response);
 	});
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-    mode: 'spa'
+  mode: 'spa', // Remove to enable SSR mode
+  css: [
+    '~/node_modules/normalize.css/normalize.css',
+    '~/styles/grid.scss',
+    '~/styles/style.scss'
+  ],
+  loading: {
+    color: '#24bbff'
+  }
 }
